03f8430e14c515fb80a90447dc429f7004705a05
Jiren Patel <jirenpatel@gmail.com>
2022-08-04 12:34:36 +0530

    up minor version

--- lib/zoho_api/version.rb
+++ lib/zoho_api/version.rb
@@ -4,29 +4,29 @@ module ZohoApi
   class Version
     MAJOR = 0 unless defined? ZohoApi::Version::MAJOR
     MINOR = 2 unless defined? ZohoApi::Version::MINOR
-    PATCH = 3 unless defined? ZohoApi::Version::PATCH
+    PATCH = 6 unless defined? ZohoApi::Version::PATCH
 
     class << self
       # @return [String]
       def to_s
-        File.exist?(filename) ? from_file : from_version      
+        File.exist?(filename) ? from_file : from_version
       end
-      
+
       def from_file
-        File.open(filename).read.strip        
-      end  
-      
+        File.open(filename).read.strip
+      end
+
       def from_version
         [MAJOR, MINOR, PATCH].compact.join('.')
-      end  
-      
+      end
+
       def filepath
         File.expand_path('zoho_api/VERSION', __dir__)
       end
-      
+
       def filename
         'VERSION'
-      end    
+      end
     end
   end
 end
--- spec/zoho_api_spec.rb
+++ spec/zoho_api_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe ZohoApi do
   it 'has a version number' do
-    expect(ZohoApi::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(ZohoApi::Version.from_file).not_to be_empty
   end
 end
